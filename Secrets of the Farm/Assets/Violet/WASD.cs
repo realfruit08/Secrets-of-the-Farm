7354789ab8c3d5a94505b2116ae542387077dff4
realfruit08 <mick.mcnulty08@gmail.com>
2025-03-29 18:21:10 -0400

    Start of Unity Project
    Here are the character and some movement controls. I added health (sort of)

---
+++ Secrets of the Farm/Assets/Violet/WASD.cs
@@ -0,0 +1,63 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Movement : MonoBehaviour
+{
+    public float Health = 100f;
+    public float HealthAtStart = 100f;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Health = HealthAtStart;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 4.0f;
+        float run = 3.0f;
+        float sneak = 0.5f;
+        if (Health <= 0)
+        {
+            Debug.Log("you are dead now");
+            SceneManager.LoadScene("Scenes/Secrets of the Farm");
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 3.0f * run;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            speed = 4.0f * sneak;
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+            spriteRenderer.flipX = false;
+        else if (Input.GetKey(KeyCode.A))
+            spriteRenderer.flipX = true;
+
+        
+    }
+    
+}
+
