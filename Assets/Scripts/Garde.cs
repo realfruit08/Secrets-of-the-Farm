5b89cf237efee8e1bee84643fce9447ae112a73d
realfruit08 <mick.mcnulty08@gmail.com>
2025-03-31 11:06:02 -0400

    More Minigames

---
+++ Assets/Scripts/Garde.cs
@@ -0,0 +1,30 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Garde : MonoBehaviour
+{
+    public GameObject player;
+    public float cropsWatered = 0;
+
+    void Start()
+    {
+        GameObject[] temp = GameObject.FindGameObjectsWithTag("Violet");
+        player = temp[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cropsWatered == 14)
+        {
+            player.GetComponent<Movement>().speed = 4;
+            player.GetComponent<Movement>().run = 3;
+            player.GetComponent<Movement>().sneak = 0.5f;
+            Destroy(gameObject);
+        }
+    }
+    public void Watered()
+    {
+        cropsWatered++;
+    }
+}
