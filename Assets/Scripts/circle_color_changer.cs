c8af4fd655d0280d34d4ea87fa6c1d5bfd542791
Gryph0123 <gryphninja@gmail.com>
2025-04-04 15:12:18 -0400

    Made the circle change color and edited the scene

--- Assets/Scripts/circle_color_changer.cs
+++ Assets/Scripts/circle_color_changer.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 public class circle_color_changer : MonoBehaviour
 {
 
-    // Gameobjects for the circle
-    
+    // Gameobjects for the circles
+    public GameObject circle1;
+    public GameObject circle2;
+
+    // Sight script
+    public PlayerIsSeen playerIsSeen;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +19,13 @@ public class circle_color_changer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerIsSeen.playerIsSeen) {
+            circle1.SetActive(true);
+            circle2.SetActive(false);
+        }
+        else {
+            circle1.SetActive(false);
+            circle2.SetActive(true);
+        }
     }
 }
